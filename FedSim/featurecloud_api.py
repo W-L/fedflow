92dacf5f53763c42368a9c8eb817023106ffbf77
W-L <weilguny.lukas@gmail.com>
2025-11-14 16:27:07 +0100

    a days development on the api

--- FedSim/featurecloud_api.py
+++ FedSim/featurecloud_api.py
@@ -1,8 +1,9 @@
 from typing import List
-import httpx
 import os
 from pathlib import Path
+import time
 
+import httpx
 from dotenv import load_dotenv
 
 # %%
@@ -29,55 +30,63 @@ class Controller:
             return False
 
 
-    def upload_files_raw(self, project_id: int, filepaths: List[str]):
-        results = {}
-        for idx, filepath in enumerate(filepaths):
-            path = Path(filepath)
-            file_name = path.name
-            # finalize_flag = "true" if idx == len(filepaths) - 1 else ""
-            params = {
-                "projectId": project_id,
-                "fileName": file_name,
-                "finalize": "",
-                "consent": ""
-            }
-            with open(path, "rb") as f:
-                r = httpx.post(f"{self.host}/file-upload/", params=params, content=f.read())
-                r.raise_for_status()
-                results[file_name] = r.text  # or r.json() if backend returns JSON
-        return results
+class Project: 
+
+    def __init__(self, project_id: str, client):
+        self.project_id = project_id
+        self.client = client
+        print(f"Project status: {self.get_status()}")
 
 
-
-    def get_available_files(self):
-        r = httpx.get(f"{self.host}/get-files-from-mounted-volume/")
+    def get_status(self) -> str:
+        r = self.client.get(f"/api/projects/{self.project_id}/")
         r.raise_for_status()
-        return r.json()  # likely a list of file names or dicts
+        data = r.json()
+        status = data.get("status")
+        return status
+
+
+    def set_status(self, status: str):
+        status_change = {"status": status}
+        r = self.client.put(f"/api/projects/{self.project_id}/", json=status_change)
+        r.raise_for_status()
+        return r.json()
+    
+
+    def is_ready(self) -> bool:
+        status = self.get_status()
+        is_ready = status == "ready"
+        return is_ready
+
+
+    def is_prepping(self) -> bool:
+        status = self.get_status()
+        is_prepping = status == "prepare"
+        return is_prepping
+    
+
+    def reset_project(self):
+        self.set_status("ready")
+        assert self.is_ready(), "Failed to reset project to ready."
+        return True
 
 
 
 
-class FCC:
-    def __init__(self, username):
-        # verify that controller is running
-        self._verify_controller()
-        self.client = httpx.Client(base_url="https://featurecloud.ai")
+class User: 
+
+    def __init__(self, username, client):
         self.username = os.getenv(username)
         self.password = os.getenv(f"{username}_P")
         assert self.username is not None, f"Environment variable {username} not set."
         assert self.password is not None, f"Environment variable {username}_P not set."
         self.access = None
         self.refresh = None
-        
-
-
-    def _verify_controller(self):
-        self.controller = Controller()
-        err_msg = "FeatureCloud controller is not running. Make sure to start it first."
-        assert self.controller.controller_is_running(), err_msg
+        self.client = client
 
 
     def login(self):
+        print(f"Logging in user {self.username}...")
         r = self.client.post("/api/auth/login/",
                              json={"username": self.username, "password": self.password})
         r.raise_for_status()
@@ -97,17 +106,30 @@ class FCC:
     def is_logged_in(self) -> bool:
         try:
             r = self.client.get("/api/user/info/")
-            return r.status_code == 200
+            ok = r.status_code == 200
+            print(f"User {self.username} logged in: {ok}")
+            return ok
         except httpx.HTTPError:
             return False
-        
 
-    def project_is_ready(self, project_id: str) -> bool:
-        r = self.client.get(f"/api/projects/{project_id}/")
-        r.raise_for_status()      # raise error if project doesn't exist
-        data = r.json()
-        is_ready = data.get("status") == "ready"
-        return is_ready
+
+
+class FCC:
+    def __init__(self, username, project_id):
+        # verify that controller is running
+        self.controller = Controller()
+        err_msg = "FeatureCloud controller is not running. Make sure to start it first."
+        assert self.controller.controller_is_running(), err_msg
+        # create HTTP client
+        self.client = httpx.Client(base_url="https://featurecloud.ai")
+        # log in as user
+        self.user = User(username=username, client=self.client)
+        self.user.login()
+        self.user.is_logged_in()
+        # project object
+        self.project = Project(project_id=project_id, client=self.client)
+        
+    
 
 
     def is_project_coordinator(self, project_id: str) -> bool:
@@ -119,28 +141,146 @@ class FCC:
         return is_coordinator
 
 
-    def set_project_prepare(self, project_id: str):
-        status_change = {"status": "prepare"}
-        r = self.client.put(f"/api/projects/{project_id}/", json=status_change)
+
+    def upload_files(self, filepaths: List[str]):
+        # checking that we are in prepare mode
+        is_prepping = self.project.is_prepping()
+        if not is_prepping:
+            print(f"Project {self.project.project_id} not in 'prepare' mode. Setting it now.")
+            self.project.set_status("prepare")
+            time.sleep(2)  # wait a bit for status to update
+
+        results = {}
+        headers = {
+            "Origin": "https://featurecloud.ai",
+            "Accept": "application/json, text/plain, */*"
+        }
+
+        for idx, filepath in enumerate(filepaths):
+            path = Path(filepath)
+            file_name = path.name
+            # finalize_flag = "true" if idx == len(filepaths) - 1 else ""
+            params = {
+                "projectId": self.project.project_id,
+                "fileName": file_name,
+                "finalize": "",
+                "consent": ""
+            }
+            with open(path, "rb") as f:
+                r = self.controller.client.post("/file-upload/", params=params, content=f.read(), headers=headers)
+                r.raise_for_status()
+                results[file_name] = r.text  # or r.json() if backend returns JSON
+        return results
+
+
+
+
+    def start_project(self):
+        """
+        Signal the controller to finalize and start the project.
+        No file content is sent; only the finalize flag is set.
+        """
+        params = {
+            "projectId": self.project.project_id,
+            "fileName": "",     # can be empty if no file is sent
+            "finalize": "true", # triggers processing
+            "consent": ""       # keep consistent with upload requests
+        }
+        headers = {
+            "Origin": "https://featurecloud.ai",
+            "Accept": "application/json, text/plain, */*"
+        }
+
+        r = self.controller.client.post("/file-upload/", params=params, headers=headers, content=b"")
         r.raise_for_status()
-        return r.json()
+        # go into project monitoring mode
+        end_status = self.monitor_project()
+        print(end_status)
+        return end_status
+
+
+
+    def monitor_project(self, interval=5, timeout=600):
+        """
+        Poll the project status until it changes from 'running'.
+        - interval: seconds between polls
+        - timeout: max total time in seconds
+        """
+        start_time = time.time()
+        
+        while True:
+            # GET project info
+            status = self.project.get_status()
+            print(f"Project {self.project.project_id} status: {status}")
+
+            if status == 'prepare':
+                time.sleep(interval)
+                continue
+            
+            if status != "running":
+                return status  # finished, failed, or any other state
+            
+            if time.time() - start_time > timeout:
+                raise TimeoutError(f"Project {self.project.project_id} did not finish within {timeout} seconds.")
+
+            time.sleep(interval)
+
+
+
+
+
+    def _get_project_runs(self):
+        r = self.controller.client.get("/project-runs/", params={"projectId": self.project.project_id})
+        r.raise_for_status()
+        r_json = r.json()
+        return r_json
+
+
+
+    def _download_file(self, endpoint, filetype, out_dir, run, step):
+        params = {"projectId": self.project.project_id, "step": step, "run": run}
+        r = self.controller.client.get(endpoint, params=params)
+        r.raise_for_status()
+
+        filename = f"p{self.project.project_id}_r{run}_s{step}.{filetype}"
+        filepath = Path(out_dir) / filename
+        with open(filepath, "wb") as f:
+            f.write(r.content)
+        print(f"Downloaded {filepath}")
+        return filepath
     
 
-    def project_is_prepping(self, project_id: str) -> bool:
-        r = self.client.get(f"/api/projects/{project_id}/")
-        r.raise_for_status()      # raise error if project doesn't exist
-        data = r.json()
-        is_prepping = data.get("status") == "prepare"
-        return is_prepping
+    
 
+    def download_outcome(self, out_dir):
+        Path(out_dir).mkdir(exist_ok=True, parents=True)
+        runs = self._get_project_runs()
+        print(f"Downloading files for project {self.project.project_id}...")
+        most_recent = runs[0]  # assuming runs are sorted by recency
+        print(f"Found {len(runs)} runs. Downloading most recent run, started on {most_recent['startedOn']}")
 
-    def stop_workflow(self, project_id: str):
-        status_change = {"status": "ready"}
-        r = self.client.put(f"/api/projects/{project_id}/", json=status_change)
-        r.raise_for_status()
-        assert self.project_is_ready(project_id), "Failed to reset project to ready."
-        return r.json()
+        
+       
+        for step in most_recent.get("logSteps", []):
+            logpath = self._download_file(
+                endpoint="/logs-download/",
+                filetype="log",
+                out_dir=out_dir,
+                run=most_recent['runNr'],
+                step=step
+            ) 
+        
 
+        for step in most_recent.get("resultSteps", []):
+            resultpath = self._download_file(
+                endpoint="/file-download/",
+                filetype="zip",
+                out_dir=out_dir,
+                run=most_recent['runNr'],
+                step=step
+            )
+            
+        return 
 
 
 
@@ -148,35 +288,28 @@ class FCC:
 #%%
 
 
-user = FCC(username="client0")
-user.login()
-
-
-# print(user.project_is_ready(project_id="17268"))
-# print(user.is_project_coordinator(project_id="17268"))
+fcc = FCC(username="client0", project_id="17268")
 
 
 # %%
-print(user.project_is_prepping(project_id="17268"))
-
-user.set_project_prepare(project_id="17268")
-
-print(user.project_is_prepping(project_id="17268"))
-
-user.stop_workflow(project_id="17268")
-
-print(user.project_is_ready(project_id="17268"))
+# fcc.upload_files(filepaths=["test_data/svd_solo/config.yaml",
+                            # "test_data/svd_solo/mnist_200.tsv"])
+# fcc.start_project()
 
 
 
 #%%
 
-cont = Controller()
+# fcc.project.reset_project()
 
-cont.controller_is_running()
-cont.upload_files_raw(project_id=17268, filepaths=["data/federated_svd_split/mnist_split_0.tsv", "data/some_more_testing"])
 
-files = cont.get_available_files()
-print(files)
+#%%
+
+
+
+
+#%%
+fcc.download_outcome(out_dir="results/")
+
 
 # %%
