6d0fc9a6a014cac2dbfd9b8e9449b8bac4cd0d82
W-L <weilguny.lukas@gmail.com>
2026-01-09 12:14:23 +0100

    add note

--- scripts/provision_biosphere.sh
+++ scripts/provision_biosphere.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-# the ubuntu box on biosphere already has python (3.12) and docker installed
+# the ubuntu box on biosphere already has python (3.12) and docker (28.0.1) installed
+# if the biosphere box updates docker it might stop working
 # the only thing we need is venv to install and run fedsim
 sudo apt-get update && sudo apt-get install -y python3.12-venv
 
